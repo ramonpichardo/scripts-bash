bb927189282806ca1cb9e3340de746926d4f37b8
Ramon Pichardo <ramonpichardo+github@gmail.com>
2018-06-30 03:04:18 -0700

    Add rsync options z, P, stats, human-readable

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-rsync -a --progress /Volumes/Storage01/books /Volumes/Backup01
-rsync -a --progress /Volumes/Storage01/documents /Volumes/Backup01
-rsync -a --progress /Volumes/Storage01/downloads /Volumes/Backup01
-rsync -a --progress /Volumes/Storage01/movies /Volumes/Backup01
-rsync -a --progress /Volumes/Storage01/music /Volumes/Backup01
-rsync -a --progress /Volumes/Storage01/pictures /Volumes/Backup01
-rsync -a --progress /Volumes/Storage01/software /Volumes/Backup01
+rsync -azP --stats --human-readable /Volumes/Storage01/books /Volumes/Backup01
+rsync -azP --stats --human-readable /Volumes/Storage01/documents /Volumes/Backup01
+rsync -azP --stats --human-readable /Volumes/Storage01/downloads /Volumes/Backup01
+rsync -azP --stats --human-readable /Volumes/Storage01/movies /Volumes/Backup01
+rsync -azP --stats --human-readable /Volumes/Storage01/music /Volumes/Backup01
+rsync -azP --stats --human-readable /Volumes/Storage01/pictures /Volumes/Backup01
+rsync -azP --stats --human-readable /Volumes/Storage01/software /Volumes/Backup01
 exit 0
