9e2223a4ec83b0027bc90de1a8c176e2834dcac8
Ramon Pichardo <ramonpichardo+github@gmail.com>
2018-06-30 01:13:49 -0700

    Update shebang to use env for greater portability

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 rsync -a --progress /Volumes/Storage01/books /Volumes/Backup01
 rsync -a --progress /Volumes/Storage01/documents /Volumes/Backup01
 rsync -a --progress /Volumes/Storage01/downloads /Volumes/Backup01
--- crontab.sh
+++ crontab.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 0 23 * * * . $HOME/.bash_profile; /path/to/command/or/script/to/run
