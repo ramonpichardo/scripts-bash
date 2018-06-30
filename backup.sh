4a0b00ef2049b78afd406a0f265940cf7af87884
Ramon Pichardo <ramonpichardo+github@gmail.com>
2018-06-30 00:48:17 -0700

    Initial commit of scripts to repo

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rsync -a --progress /Volumes/Storage01/books /Volumes/Backup01
+rsync -a --progress /Volumes/Storage01/documents /Volumes/Backup01
+rsync -a --progress /Volumes/Storage01/downloads /Volumes/Backup01
+rsync -a --progress /Volumes/Storage01/movies /Volumes/Backup01
+rsync -a --progress /Volumes/Storage01/music /Volumes/Backup01
+rsync -a --progress /Volumes/Storage01/pictures /Volumes/Backup01
+rsync -a --progress /Volumes/Storage01/software /Volumes/Backup01
+exit 0
---
+++ crontab.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+0 23 * * * . $HOME/.bash_profile; /path/to/command/or/script/to/run
