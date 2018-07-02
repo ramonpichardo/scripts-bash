c679e86d1168056587347273a432d9b223f7ba36
Ramon <ramonpichardo+github@gmail.com>
2018-07-02 16:52:38 -0700

    Update bash profile setup and add xcode lic access

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-echo "export PATH="$PATH:~/scripts"" >> ~/.bash_profile
-echo "export PS1="\u@\h [\w] $ ""  >> ~/.bash_profile
-echo "alias ll="ls -laFGh"" >> ~/.bash_profile
-echo "alias nmap="nmap -vv -O -P0 -sTUV --top-ports 1000"" >> ~/.bash_profile
-echo "alias socks5="ssh -D 40022 -f -C -q -N ramon@192.168.1.100"" >> ~/.bash_profile
-echo "alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"" >> ~/.bash_profile
+cat >> ~/.bash_profile <<'EOF'
+> export PATH="$PATH:~/scripts"
+> export PS1="\u@\h [\w] $ "
+> alias ll="ls -laFGh"
+> alias nmap="nmap -vv -O -P0 -sTUV --top-ports 1000"
+> alias socks5="ssh -D 40022 -f -C -q -N ramon@192.168.1.100"
+> alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+> EOF
+
+source ~/.bash_profile
 
 softwareupdate -ia
+
 xcode-select --install
+
+xcodebuild -license
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew doctor
+
 reboot
