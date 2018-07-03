6ed92bf7bec751ecc44d54ed69c3edb2f074390c
Ramon <ramonpichardo+github@gmail.com>
2018-07-03 14:42:06 -0700

    Add how-to-use comments

--- crontab.sh
+++ crontab.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
+
+# minute (0 - 59)
+# hour (0 - 23)
+# day of month (1 - 31)
+# month (1 - 12 or jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
+# day of week (0 - 6 or sun,mon,tue,wed,thu,fri,sat)
+# path-to-command-to-be-executed
+# Note: ". $HOME/.bash_profile; " means "source the current user's bash profile (prior to the job task execution)"
+
 0 23 * * * . $HOME/.bash_profile; /path/to/command/or/script/to/run
