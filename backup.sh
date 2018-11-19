25eee3bdc271ac415ae1c444840f2d5c2e320e2d
Ramon <ramonpichardo+github@gmail.com>
2018-11-19 00:19:19 -0800

    Add comments for flags used: a,z,P,stats,h-r

--- backup.sh
+++ backup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# -a = archive mode
+# -z = compress file data during transfer
+# -P = same as --partial (keep partially transferred files) --progress (show progress during transfer)
+# --stats = give some file-transfer stats
+# --human-readable = output numbers in a human-readable format
 rsync -azP --stats --human-readable /Volumes/Storage01/books /Volumes/Backup01
 rsync -azP --stats --human-readable /Volumes/Storage01/documents /Volumes/Backup01
 rsync -azP --stats --human-readable /Volumes/Storage01/downloads /Volumes/Backup01
