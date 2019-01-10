6d10293c110fff74fb02cb3f2c2676c3058cec33
Ramon <ramonpichardo+github@gmail.com>
2019-01-09 23:33:05 -0800

    Add rsync options -c,-h,-v

--- backup.sh
+++ backup.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
+# -c = checksum, skip based on checksum, not mod-time & size
+# -h = output numbers in a human-readable format
 # -a = archive mode
+# -v = verbose, increase verbosity
 # -z = compress file data during transfer
 # -P = same as --partial (keep partially transferred files) --progress (show progress during transfer)
 # --stats = give some file-transfer stats
-# --human-readable = output numbers in a human-readable format
 # 
 # Copying directories from local volumes to other local volumes
-rsync -azP --stats --human-readable /Volumes/Storage01/books /Volumes/Backup01
-rsync -azP --stats --human-readable /Volumes/Storage01/documents /Volumes/Backup01
-rsync -azP --stats --human-readable /Volumes/Storage01/downloads /Volumes/Backup01
-rsync -azP --stats --human-readable /Volumes/Storage01/movies /Volumes/Backup01
-rsync -azP --stats --human-readable /Volumes/Storage01/music /Volumes/Backup01
-rsync -azP --stats --human-readable /Volumes/Storage01/pictures /Volumes/Backup01
-rsync -azP --stats --human-readable /Volumes/Storage01/software /Volumes/Backup01
+rsync -chavzP --stats /Volumes/Storage01/books /Volumes/Backup01
+rsync -chavzP --stats /Volumes/Storage01/documents /Volumes/Backup01
+rsync -chavzP --stats /Volumes/Storage01/downloads /Volumes/Backup01
+rsync -chavzP --stats /Volumes/Storage01/movies /Volumes/Backup01
+rsync -chavzP --stats /Volumes/Storage01/music /Volumes/Backup01
+rsync -chavzP --stats /Volumes/Storage01/pictures /Volumes/Backup01
+rsync -chavzP --stats /Volumes/Storage01/software /Volumes/Backup01
 # 
 # Copy a file from local server to a remote server
-rsync -azP --stats --human-readable /srv/iso/CentOS-7-x86_64-DVD-1804.iso ramon@192.168.1.11:/Users/ramon/srv/ISO-nix/
+rsync -chavzP --stats /srv/iso/CentOS-7-x86_64-DVD-1804.iso ramon@192.168.1.11:/Users/ramon/srv/ISO-nix/
 exit 0
