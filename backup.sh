dfcd15e590e17048092c7b96b2590f5477fa9fff
Ramon <ramonpichardo+github@gmail.com>
2019-01-30 16:48:03 -0800

    Prepend rsync with 'nohup nice' then append a &

--- backup.sh
+++ backup.sh
@@ -8,14 +8,14 @@
 # --stats = give some file-transfer stats
 # 
 # Copying directories from local volumes to other local volumes
-rsync -chavzP --stats /Volumes/Storage01/books /Volumes/Backup01
-rsync -chavzP --stats /Volumes/Storage01/documents /Volumes/Backup01
-rsync -chavzP --stats /Volumes/Storage01/downloads /Volumes/Backup01
-rsync -chavzP --stats /Volumes/Storage01/movies /Volumes/Backup01
-rsync -chavzP --stats /Volumes/Storage01/music /Volumes/Backup01
-rsync -chavzP --stats /Volumes/Storage01/pictures /Volumes/Backup01
-rsync -chavzP --stats /Volumes/Storage01/software /Volumes/Backup01
+nohup nice rsync -chavzP --stats /Volumes/Storage01/books /Volumes/Backup01 &
+nohup nice rsync -chavzP --stats /Volumes/Storage01/documents /Volumes/Backup01 &
+nohup nice rsync -chavzP --stats /Volumes/Storage01/downloads /Volumes/Backup01 &
+nohup nice rsync -chavzP --stats /Volumes/Storage01/movies /Volumes/Backup01 &
+nohup nice rsync -chavzP --stats /Volumes/Storage01/music /Volumes/Backup01 &
+nohup nice rsync -chavzP --stats /Volumes/Storage01/pictures /Volumes/Backup01 &
+nohup nice rsync -chavzP --stats /Volumes/Storage01/software /Volumes/Backup01 &
 # 
 # Copy a file from local server to a remote server
-rsync -chavzP --stats /srv/iso/CentOS-7-x86_64-DVD-1804.iso ramon@192.168.1.11:/Users/ramon/srv/ISO-nix/
+nohup nice rsync -chavzP --stats /srv/iso/CentOS-7-x86_64-DVD-1804.iso ramon@192.168.1.11:/Users/ramon/srv/ISO-nix/ &
 exit 0
