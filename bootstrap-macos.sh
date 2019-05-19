79e2ee9c9ea0af22aa579c0857a625c24ed144d8
Ramon <ramonpichardo+github@gmail.com>
2019-05-18 21:22:18 -0700

    Add exports for PATH and HISTORY

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -3,6 +3,10 @@
 cat >> ~/.bash_profile <<'EOF'
 > export PATH="$PATH:~/scripts"
 > export PS1="\u@\h [\w] $ "
+> export HISTTIMEFORMAT="%Y-%m-%d %T "
+> export HISTCONTROL=ignorespace:ignoredups
+> export HISTSIZE=1000
+> export HISTFILESIZE=1000
 > alias ll="ls -laFGh"
 > alias nmap="nmap -vv -O -P0 -sTUV --top-ports 1000"
 > alias socks5="ssh -D 40022 -f -C -q -N ramon@192.168.1.100"
