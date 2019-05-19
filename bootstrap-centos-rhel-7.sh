b1de07eeff14e831edf7aa68d2d0c3fe750ff17a
Ramon <ramonpichardo+github@gmail.com>
2019-05-18 21:48:53 -0700

    Install latest mainline kernel

--- bootstrap-centos-rhel-7.sh
+++ bootstrap-centos-rhel-7.sh
@@ -7,7 +7,7 @@ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 yum install yum-plugin-fastestmirror
 
 # Upgrade Linux kernel
-yum --enablerepo=elrepo-kernel install kernel-ml-4.*
+yum --enablerepo=elrepo-kernel install kernel-ml-5.*
 
 # Update a CentOS or Red Hat system
 yum -y update && yum -y upgrade
